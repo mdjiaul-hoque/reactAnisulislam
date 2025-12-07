612ec207fc50aaa70ebb42699dc9c2b25df7463f
mdjiaul-hoque <jiasofte@gmail.com>
2025-12-07 20:00:32 +0600

    nested mapping form json data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card1 from './components/Card1';
+// import Card1 from './components/Card1';
 import Data from './data.json'
 
 const App = () => {
@@ -8,15 +8,28 @@ const App = () => {
 console.log(Data);
 
 
-
- 
-
   return (
     <div>
-      <h1 className="handlingStyle">Todo App </h1>
+      <h1 className="handlingStyle">Nested mapping </h1>
      
       {
-        Data.map((item)=> <Card1 toTitle = {item.title} toDes = {item.desc} /> )
+        Data.map((item, index)=> 
+        <article key={index}>
+          <h3>fullName : {item.fullName}</h3>
+          <p>age: { item.age}</p>
+           
+            {
+
+              item.phones.map((phone,index)=>
+            <div key={index}> 
+              <p>home:{phone.home}</p> 
+              <p>office:{phone.office}</p> 
+            </div> 
+              )
+            }
+
+        </article> 
+        )
       }
      
     </div>
