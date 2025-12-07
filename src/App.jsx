775ee65484c3cad1d14b06d62fa6d23701d7d086
mdjiaul-hoque <jiasofte@gmail.com>
2025-12-07 19:27:12 +0600

    json data handle formate 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Card1 from './components/Card1';
+import Data from './data.json'
 
 const App = () => {
+
+
+console.log(Data);
+
   return (
     <div>
-      <h1 className="handlingStyle">Todo App</h1>
-      <Card1 toTitle = " call Vite" toDes = "hasbi allah o niyemal okil niyemal mawla o niyemal nasir" />
-      <Card1 toTitle = " call Veu" toDes = "hasbi allah o niyemal okil niyemal mawla o niyemal nasir" />
-      <Card1 toTitle = " call Anguler" toDes = "hasbi allah o niyemal okil niyemal mawla o niyemal nasir" />
-      <Card1 toTitle = " call Nextjs" toDes = "hasbi allah o niyemal okil niyemal mawla o niyemal nasir" />
-      
+      <h1 className="handlingStyle">Todo App </h1>
+     
+      <Card1 toTitle = {Data[0].title} toDes = {Data[0].desc} />
       
     </div>
-  );
+  )
 };
 
 export default App;
